e34a840df14e97a8111c641a67ab3ddf72c03da4
Bat-Zion Rotman <brotman@redhat.com>
2025-10-21 13:07:51 +0300

    Revert "RHWA-89: resolve broken plugin in hypershift cluster"

--- src/components/list/NodeHealthCheckListPage.tsx
+++ src/components/list/NodeHealthCheckListPage.tsx
@@ -16,18 +16,49 @@ import { NodeHealthCheck } from "data/types";
 import Modals from "components/modals/Modals";
 import { Selector } from "@openshift-console/dynamic-plugin-sdk-internal/lib/api/common-types";
 import { ModalsContextProvider } from "components/modals/ModalsContext";
+import { Alert, Button } from "@patternfly/react-core";
+import { useNodeHealthChecksDisabled } from "apis/nodeHealthCheckApis";
 import { NodeHealthchecksTable } from "./NodeHealthCheckTable";
+import { useNodeHealthCheckNavigation } from "navigation/useNodeHealthCheckNavigation";
 import { withFallback } from "copiedFromConsole/error";
+import { StatusBox } from "copiedFromConsole/utils/status-box";
 import "./nhc-list.css";
 
 type ListPageProps = {
   selector?: Selector;
 };
 
-const NodeHealthCheckCreate = () => {
+const DisabledAlert: React.FC = () => {
+  const { t } = useNodeHealthCheckTranslation();
+  const navigation = useNodeHealthCheckNavigation();
+  return (
+    <Alert variant="info" isInline title={t("NodeHealthChecks are disabled")}>
+      {t(
+        "NodeHealthChecks are not available because MachineHealthChecks are already enabled. To make edits, go to"
+      )}{" "}
+      <Button
+        variant="link"
+        isInline
+        onClick={() => navigation.gotoMachineHealthChecks()}
+      >
+        {t("MachineHealthChecks page.")}
+      </Button>
+    </Alert>
+  );
+};
+
+const NodeHealthCheckCreate: React.FC<{ isDisabled: boolean }> = ({
+  isDisabled,
+}) => {
   const { t } = useNodeHealthCheckTranslation();
   const label = t("Create NodeHealthCheck");
-  return (
+  return isDisabled ? (
+    <div>
+      <Button variant="primary" isDisabled>
+        {label}
+      </Button>
+    </div>
+  ) : (
     <ListPageCreate groupVersionKind={nodeHealthCheckStringKind}>
       {label}
     </ListPageCreate>
@@ -43,30 +74,40 @@ const NodeHealthCheckListPage_: React.FC<ListPageProps> = ({ selector }) => {
     namespaced: false,
     selector,
   });
+  const [isDisabled, disabledLoaded, disabledError] =
+    useNodeHealthChecksDisabled();
 
   const [data, filteredData, onFilterChange] =
     useListPageFilter(nodeHealthChecks);
   const { t } = useNodeHealthCheckTranslation();
+
   return (
-    <ModalsContextProvider>
-      <ListPageHeader title={t("NodeHealthChecks")}>
-        <NodeHealthCheckCreate />
-      </ListPageHeader>
-      <ListPageBody>
-        <ListPageFilter
-          data={data}
-          loaded={loaded}
-          onFilterChange={onFilterChange}
-        />
-        <NodeHealthchecksTable
-          data={filteredData}
-          unfilteredData={nodeHealthChecks}
-          loaded={loaded}
-          loadError={loadError}
-        />
-      </ListPageBody>
-      <Modals />
-    </ModalsContextProvider>
+    <StatusBox
+      loaded={disabledLoaded}
+      loadError={disabledError}
+      data={{ isDisabled: isDisabled }}
+    >
+      <ModalsContextProvider>
+        <ListPageHeader title={t("NodeHealthChecks")}>
+          <NodeHealthCheckCreate isDisabled={isDisabled} />
+        </ListPageHeader>
+        <ListPageBody>
+          {isDisabled && <DisabledAlert />}
+          <ListPageFilter
+            data={data}
+            loaded={loaded}
+            onFilterChange={onFilterChange}
+          />
+          <NodeHealthchecksTable
+            data={filteredData}
+            unfilteredData={nodeHealthChecks}
+            loaded={loaded}
+            loadError={loadError}
+          />
+        </ListPageBody>
+        <Modals />
+      </ModalsContextProvider>
+    </StatusBox>
   );
 };
 
